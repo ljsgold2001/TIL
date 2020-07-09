e67ee857b5409ec1dff17dbf2ba05d49fdd14254
leejisoo <ljsgold2001@naver.com>
2020-07-09 16:19:02 +0900

    [ADD]객체생성

---
+++ OOP/OOP1/src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	public int StudentID;
+	public String studentName;
+	public String address; //여기까지 맴버 변수이다.
+	
+	public void showStudentInfo() { //여기는 메서
+		System.out.println(studentName + " , " + address );
+		
+	}
+	
+}
+//이 클래스는 속성의 값이 아직 생성이 되지 않았기때문에 이제 객체를 생성
+
---
+++ OOP/OOP1/src/StudentTest.java
@@ -0,0 +1,16 @@
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student(); //객체 생성(생성자)
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		studentLee.showStudentInfo();
+		
+ 				
+		
+		
+	}
+}
