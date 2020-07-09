66091287021825ecd6f18f4b28daaeea31ee0744
leejisoo <ljsgold2001@naver.com>
2020-07-09 19:26:32 +0900

    [ADD]싱글톤예제

---
+++ OOP/OOP8/src/CardCompany.java
@@ -0,0 +1,23 @@
+
+public class CardCompany {
+	
+	private static CardCompany instance = new CardCompany();
+	
+	private CardCompany() {
+		
+	}
+	
+	public static CardCompany getInstance() {
+		
+		return instance;
+	}
+	
+	public Card createCard() {
+		Card card = new Card();
+		
+		return card;
+	}
+	
+	
+
+}
